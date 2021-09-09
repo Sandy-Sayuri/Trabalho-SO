48ca36a9d0a8c7946b14fee15b74d6e9e611f454
Sandy Sayuri Katsumata Ishii <66523335+Sandy-Sayuri@users.noreply.github.com>
2021-09-08 21:50:22 -0300

    n sei se está certo

--- teste.py
+++ teste.py
@@ -1,16 +1,16 @@
 #emdereçamento em 2 niveis
 import random
-Memoria_logica=["P0","P1","P2","P3",]
-tabela_de_pag=[6,2,1,0]
-Memoria_fisica=["P3","P2","P1","P0"]
+Memoria_logica=["P0","P1","P2","P3","P4"]
+tabela_de_pag=[4,2,1,0,3]
+Memoria_fisica=["P3","P2","P1","P0","P4"]
 pg=random.randrange(0,4)
-o=random.choice(Memoria_fisica)
+o=random.choice(Memoria_logica)
 for i in tabela_de_pag:
     if(pg==i):
         fr=int(tabela_de_pag.index(i)) 
-        Memoria_fisica.append(o)#falta o index
+        safe=tabela_de_pag[i]
+        Memoria_fisica.insert(i,o)#falta o index
         print(Memoria_fisica)
-else:
-    print('ERRO')
+
 
 
