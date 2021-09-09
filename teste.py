225e0e9dbd3c0ab06d69170d4d5ce552b2ea9783
Sandy Sayuri Katsumata Ishii <66523335+Sandy-Sayuri@users.noreply.github.com>
2021-09-08 22:28:22 -0300

    acredito ter resolvido o problema

--- teste.py
+++ teste.py
@@ -1,13 +1,11 @@
 #emdereçamento em 2 niveis
-import random
-Memoria_logica=["P0","P1","P2","P3","P4"]
-tabela_de_pag=[4,2,1,0,3]
-Memoria_fisica=["P3","P2","P1","P0","P4"]
-pg=random.randrange(0,4)
-o=random.choice(Memoria_logica)
-for i in tabela_de_pag:
-    if(pg==i):
-        fr=int(tabela_de_pag.index(i)) 
-        safe=tabela_de_pag[i]
-        Memoria_fisica.insert(i,o)
-        print(Memoria_fisica)
+import random #importe o random
+Memoria_logica=["P0","P1","P2","P3","P4"]#tabela de memória lógica
+tabela_de_pag=[4,2,1,0,3]#tabela de páginas
+Memoria_fisica=["P3","P2","P1","P0","P4"]#Memofia fisica
+pg=random.choice(tabela_de_pag)#sorteando um dos items da tabela de páginas
+o=random.choice(Memoria_logica)#sorteando um dos items da tabela de memória lógica
+for i in tabela_de_pag:#aqui vai mostrar os valores da tabela de pag
+    if(pg==i):#quando a o valor de pag for igual ao que tem na lista
+        Memoria_fisica.insert(i,o)#adiciona o item na pag
+        print(Memoria_fisica)#printa a pag
