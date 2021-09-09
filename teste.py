9448e2e0238509e644fa9600affc4d5d60f6d17a
Sandy Sayuri Katsumata Ishii <66523335+Sandy-Sayuri@users.noreply.github.com>
2021-09-08 21:26:45 -0300

    numero aleatorio colocado

--- teste.py
+++ teste.py
@@ -1,9 +1,10 @@
 #emdereçamento em 2 niveis
+import random
 Memoria_logica=["P0","P1","P2","P3",]
-tabela_de_pag=[6,2,1,0,]
+tabela_de_pag=[6,2,1,0]
 Memoria_fisica=["P3","P2","P1","P0"]
-pg=int(input('escreva o numero da pagina:'))
-o=str(input('escreva o deslocamento:'))
+pg=random.randrange(0,4)
+o=random.choice(Memoria_fisica)
 for i in tabela_de_pag:
     if(pg==i):
         fr=int(tabela_de_pag.index(i)) 
